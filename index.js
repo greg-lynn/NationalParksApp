be02d7413d95f99d9a97599426d01816b2c5b436
Greg Lynn <greglynn1992@gmail.com>
2020-12-16 13:28:38 -0700

    Updates with directions as an <li>

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function displayResults(responseJson, maxResults) {
     for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
         $('.results-list').append(`<li>
         <h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
-        <p>${responseJson.data[i].description}</p>
+        <p><b>${responseJson.data[i].description}</b></p>
+        <p>${responseJson.data[i].directionsInfo}</p>
         </li>`);
     }
     $('.results').removeClass('hidden');
